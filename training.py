96302e6201d6d8042ba86fcd4f843e391bea4540
Kyla <kyla2@kylascomputer.local>
2016-11-15 12:41:51 -0500

    get training data function

--- training.py
+++ training.py
@@ -6,13 +6,19 @@
 import numpy as np
 import cv2
 
-# trainingdata = []
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def getTrainingData():
+	trainingdata = {}
+	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-for i in range(len(alphabet)):
-	filename = 'trainingdata/%s.png' % alphabet[i]
-	letter = cv2.imread(filename)
-	cv2.imshow('image',letter)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	for i in range(len(alphabet)):
+		filename = 'trainingdata/%s.png' % alphabet[i]
+		letter = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+		trainingdata[alphabet[i]] = letter
+		#cv2.imshow('image',letter)
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+	
+	return trainingdata
+	
+print(getTrainingData())
