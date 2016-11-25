9f4c4a417f4c21c5053ecfd2eb718e2728ba84fd
kylambouldin <kylambouldin@gmail.com>
2016-11-25 18:08:46 -0500

    save training data; function to respond letter

--- training.py
+++ training.py
@@ -6,65 +6,31 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-	
-def createDigitTrainingData():	
-	# import training data
-	img = cv2.imread('digits.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
-	# split the image to 5000 cells, each 20x20 size
-	cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
-	# Make it into a (50, 100, 20, 20) numpy array
-	x = np.array(cells)
-	# prepare data
-	train = x[:,:].reshape(-1,400).astype(np.float32) # Size = (2500,400) (left half)
-	#test = x[:,75:100].reshape(-1,400).astype(np.float32) # Size = (2500,400) (right half)
 
-	# create labels for train data
-	k = np.arange(10)
-	train_labels = np.repeat(k,500)[:,np.newaxis]
-	#test_labels = np.repeat(k,125)[:,np.newaxis]
-		
-	#initiate kNN, train the data
-	knn = cv2.KNearest()
-	knn.train(train, train_labels)
-
-	# test data
-# 	ret,result,neighbours,dist = knn.find_nearest(test,k=5)
-# 	matches = result==test_labels
-# 	correct = np.count_nonzero(matches)
-# 	accuracy = correct*100.0/result.size
-# 	print accuracy
-   
-	# import and reshape testing data
-	print("Testing Digits");
-	for i in range(10):
-		filename = 'testingdata/%s.png' % i		
-		testimg = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)	
-		testing = testimg[:,:].reshape(-1, 400).astype(np.float32) # 400 = 20x20	
-		test_label = i
-	
-		# test data for k = 1
-		ret,result,neighbors,dist = knn.find_nearest(testing,k=5)
-	 	print test_label
-	 	print result
-	 	
-def createLetterTrainingData():	
-	# import training data
+# ====================
+# use digital letters to create and save training data
+# ====================
+def generateLetterTrainingData():	
 	img = cv2.imread('letters.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
-	# split the image to 130 cells, each 20x20 size
 	cells = [np.hsplit(row,26) for row in np.vsplit(img,5)]
-	# Make it into a (5, 26, 20, 20) numpy array
 	x = np.array(cells)
-	# prepare data
-	train = x[:,:].reshape(-1,400).astype(np.float32) # Size = (130,400)
-
-	# create labels for train data
+	train = x[:,:].reshape(-1,400).astype(np.float32)
 	k = np.arange(26)
 	train_labels = np.repeat(k,5)[:,np.newaxis]
-		
-	#initiate kNN, train the data
-	knn = cv2.KNearest()
-	knn.train(train, train_labels)
+	np.savez('knn_letter_data.npz',train=train, train_labels=train_labels)
 	
+# ====================
+# use saved data to test alphabet
+# ====================
+def testLetters():
+	with np.load('knn_letter_data.npz') as data:
+		train = data['train']
+		train_labels = data['train_labels']
+		
+		#initiate kNN, train the data
+		knn = cv2.KNearest()
+		knn.train(train, train_labels)
+		
 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	print("Testing Alphabet");
 	for i in range(len(alphabet)):
@@ -77,6 +43,77 @@ def createLetterTrainingData():
 		ret,result,neighbors,dist = knn.find_nearest(testing,k=5)
 	 	print test_label
 	 	print result
-		
+
+# ====================
+# given 20x20 grayscale image return estimated letter (in int value)
+# ====================
+def getCorrespondingLetter(image):
+	with np.load('knn_letter_data.npz') as data:
+		train = data['train']
+		train_labels = data['train_labels']
+
+	knn = cv2.KNearest()
+	knn.train(train, train_labels)
+	
+	testimg = image[:,:].reshape(-1, 400).astype(np.float32)
+	ret, result, neighbors, dist = knn.find_nearest(testing, k=5)
+	return result
+
+
+# ====================
+# use opencv digit data to create training set for digits
+# ====================	
+def createDigitTrainingData():	
+	img = cv2.imread('digits.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+	cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
+	x = np.array(cells)
+	train = x[:,:].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+	#test = x[:,75:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+
+	k = np.arange(10)
+	train_labels = np.repeat(k,500)[:,np.newaxis]
+	#test_labels = np.repeat(k,125)[:,np.newaxis]
+
+	np.savez('knn_digit_data.npz',train=train, train_labels=train_labels)
+	# np.savez('knn_digit_test_data.npz',test=train, test_labels=test_labels)
+
+
+# ====================
+# use saved data to test digits
+# ====================
+def testDigits():
+	with np.load('knn_digit_data.npz') as data:
+		train = data['train']
+		train_labels = data['train_labels']
+	
+	knn = cv2.KNearest()
+	knn.train(train, train_labels)
+	
+	'''
+	with np.load('knn_digit_test_data.npz') as data:
+		test = data['test']
+		test_labels = data['test_labels']
+	
+	ret,result,neighbours,dist = knn.find_nearest(test,k=5)
+	matches = result==test_labels
+	correct = np.count_nonzero(matches)
+	accuracy = correct*100.0/result.size
+	print accuracy
+	'''
+
+	print("Testing Digits");
+	for i in range(10):
+		filename = 'testingdata/%s.png' % i		
+		testimg = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)	
+		testing = testimg[:,:].reshape(-1, 400).astype(np.float32) # 400 = 20x20	
+		test_label = i
+	
+		# test data for k = 1
+		ret,result,neighbors,dist = knn.find_nearest(testing,k=5)
+	 	print test_label
+	 	print result
+	 	
+generateLetterTrainingData()	 	
+testLetters()
 createDigitTrainingData()
-createLetterTrainingData()
+testDigits()
